93b05b7535c7bd957e0188c272e9c955fff55973
maainul <mainul080@hotmail.com>
2019-02-22 11:22:36 +0600

    update readme

--- README.md
+++ README.md
@@ -496,9 +496,15 @@ class OggFile(AudioFile):
 ```
 
 >> ogg = OggFile("myfile.ogg")
+
 >> ogg.play()
+
 playing myfile.ogg as ogg
+
 >> mp3 = MP3File("myfile.mp3")
+
 >> mp3.play()
+
 playing myfile.mp3 as mp3
+
 >> not_an_mp3 = MP3File("myfile.ogg")     
--- chapter_3_ploymorphism.py
+++ chapter_3_ploymorphism.py
@@ -20,10 +20,15 @@ class OggFile(AudioFile):
         print("playing {} as ogg".format(self.filename))
 '''
 >>> ogg = OggFile("myfile.ogg")
+
 >>> ogg.play()
+
 playing myfile.ogg as ogg
+
 >>> mp3 = MP3File("myfile.mp3")
+
 >>> mp3.play()
+
 playing myfile.mp3 as mp3
 >>> not_an_mp3 = MP3File("myfile.ogg")     
 '''
